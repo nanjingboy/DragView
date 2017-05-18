efb02e973caffc235b9b4df1f08d5ae352564411
Tom.Huang <hzlhu.dargon@gmail.com>
2017-05-18 14:28:29 +0800

    basic demo

--- sample/src/main/java/me/tom/dragview/sample/MainActivity.java
+++ sample/src/main/java/me/tom/dragview/sample/MainActivity.java
@@ -1,8 +1,10 @@
 package me.tom.dragview.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.basicDemoButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BasicDemoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
